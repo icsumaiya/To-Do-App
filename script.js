56e0ef9f1ad25fdb34a268beb85d749497fbf9d0
Sumaiya Islam Chowdhury <sumaiyaislamchowdhury57@gmail.com>
2026-05-15 18:30:53 +0600

    script.js

--- script.js
+++ script.js
@@ -1,36 +1,76 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const inputBox =
+document.getElementById("input-box");
+
+const listContainer =
+document.getElementById("list-container");
 
 function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something!!");
+    if(inputBox.value.trim() === ''){
+        alert(
+        "You must write something!"
+        );
     }
+
     else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        let li =
+        document.createElement("li");
+        li.innerHTML =
+        inputBox.value;
         listContainer.appendChild(li);
-        let span = document.createElement("span");
+        let span =
+        document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        saveData();
     }
     inputBox.value = "";
-    saveData();
+
 }
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
+
+listContainer.addEventListener(
+"click",
+function(e){
+    if(
+    e.target.tagName === "LI"
+    ){
+        e.target.classList.toggle(
+        "checked"
+        );
         saveData();
     }
-     else if(e.target.tagName === "SPAN"){
+
+    else if(
+    e.target.tagName === "SPAN"
+    ){
         e.target.parentElement.remove();
+
         saveData();
-     }
-}, false);
+    }
+},
+false
+);
 
 function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+    localStorage.setItem(
+    "data",
+    listContainer.innerHTML
+    );
 }
+
 function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
+    listContainer.innerHTML =
+    localStorage.getItem("data");
 }
+
 showTask();
+
+inputBox.addEventListener(
+"keydown",
+function(e){
+    if(e.key === "Enter"){
+
+        addTask();
+
+    }
+
+});
